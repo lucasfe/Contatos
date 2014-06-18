a492d460e9bcc64a0a512dbad4c955d548875be0
ios4357 <ios4357@caelum57-02.local>
2014-06-18 19:59:39 -0300

    Initial Commit

---
+++ Contatos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Contatos
+//
+//  Created by ios4357 on 18/06/14.
+//  Copyright (c) 2014 lgrmobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
